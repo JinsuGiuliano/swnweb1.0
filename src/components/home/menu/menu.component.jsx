a543b493ab9dade848955ad20bd0fa33948d9e87
Gustavo Giuliano 친수 <jinsu.gustavo.giuliano@gmail.com>
2024-04-03 10:09:35 -0400

    add reports

--- src/components/home/menu/menu.component.jsx
+++ src/components/home/menu/menu.component.jsx
@@ -26,13 +26,30 @@ const Menu = () => {
 							>
 								Contact Us
 							</a>
+							Our Balances:
 							<a
-								href="https://firebasestorage.googleapis.com/v0/b/swn-web.appspot.com/o/Financieel%20jaarrapport%202022.pdf?alt=media&token=d6a287a2-f131-4613-b4b3-1433957668ca"
-								gs:target="blank"
+								href="https://firebasestorage.googleapis.com/v0/b/swn-web.appspot.com/o/AnnualReport.pdf?alt=media&token=49bb519b-618b-4d4a-bfcc-4cd1160af264"
+								target="blank"
 								title=""
 								className="btn anchor-link text-dark border-0 px-3"
 							>
-								Our Balance
+							2021
+							</a>/
+							<a
+								href="https://firebasestorage.googleapis.com/v0/b/swn-web.appspot.com/o/Financieel%20jaarrapport%202022.pdf?alt=media&token=1ddbb486-24fb-4221-bc57-dd590747f894"
+								target="blank"
+								title=""
+								className="btn anchor-link text-dark border-0 px-3 secondary"
+							>
+								2022
+							</a>/
+							<a
+								href="https://firebasestorage.googleapis.com/v0/b/swn-web.appspot.com/o/Annual%20Report%202023.pdf?alt=media&token=4db2fb24-f77b-4a82-8245-2997e9be92b1"
+								target="blank"
+								title=""
+								className="btn anchor-link text-dark border-0 px-3"
+							>
+							2023
 							</a>
 						</NavbarNav>
 					</div>
